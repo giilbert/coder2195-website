12e71ce4e3ac7ee22b0fc8f534d73504837b40f4
Glitch (coder2195) <none>
2020-12-18 15:31:09 +0000

    👕🚒 Updated with Glitch

--- scripts/main.js
+++ scripts/main.js
@@ -29,5 +29,79 @@ function setup(){
   $('#bar').css("display", "block")
 }
 
-window.onload = checkip(); setup();
+window.onload = () => {
+  checkip()
+  setup()
+}
 
+/*!lang: english (en), chinese (ch), french (fr), spanish (sp) 
+ portuguese (pt), dutch (nl), german (de), arabic (ar)
+ russian (ru), korean (ko), japanese (jp), hindi (hi), greek (el)
+*/
+
+const langdata = {
+  "head": {
+    "title":{
+      "en":"Coder2195's Website",
+      "ch":"Coder2195的网站",
+      "fr":"Site Web de Coder2195",
+      "sp":"Sitio web de Coder2195",
+      "pt":"Site do Coder2195",
+      "nl":"De website van Coder2195",
+      "de":"Die Website von Coder2195",
+      "ar":"موقع ويب Coder2195",
+      "ru":"Сайт Coder2195",
+      "ko":"Coder2195의 웹 사이트",
+      "jp":"Coder2195のウェブサイト",
+      "hi":"Coder2195 की वेबसाइट",
+      "el":"Ιστοσελίδα του Coder2195"
+    }
+  },
+  "body": {
+    "null-for-copying": {
+      "en":"",
+      "ch":"",
+      "fr":"",
+      "sp":"",
+      "pt":"",
+      "nl":"",
+      "de":"",
+      "ar":"",
+      "ru":"",
+      "ko":"",
+      "jp":"",
+      "hi":"",
+      "el":""
+    },
+    "title" : {
+      "en":"Welcome to my website!",
+      "ch":"欢迎来到我的网站",
+      "fr":"Bienvenue sur mon site web!",
+      "sp":"¡Bienvenidos a mi sitio web!",
+      "pt":"Bem-vindo ao meu site!",
+      "nl":"Welkom op mijn website!",
+      "de":"Willkommen auf meiner Website!",
+      "ar":"مرحبا بكم في موقع الويب الخاص بي!",
+      "ru":"Добро пожаловать на мой сайт!",
+      "ko":"내 웹 사이트에 오신 것을 환영합니다!",
+      "jp":"私のウェブサイトへようこそ!",
+      "hi":"मेरी वेबसाइट पर स्वागत है!",
+      "el":"Καλώς ήλθατε στον ιστότοπό μου!"
+    },
+    "snap": {
+      "en":"My Snap Games",
+      "ch":"我 Snap 的游戏",
+      "fr":"Mes Jeux Snap",
+      "sp":"Mis Juegos Snap",
+      "pt":"Meus Jogos Snap",
+      "nl":"My Snap-spellen",
+      "de":"Meine Snap Spiele",
+      "ar":"ألعابي Snap",
+      "ru":"Мои игры Snap",
+      "ko":"내 Snap 게임",
+      "jp":"私の Snap ゲーム",
+      "hi":"मेरे Snap खेल",
+      "el":"Τα Snap παιχνίδια μου"
+    },
+  }
+}
